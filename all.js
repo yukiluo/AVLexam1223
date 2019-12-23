3aff687a04bb5c0ee1e5e2f94034c5893b8858f0
LUO YUCHI <103306076@nccu.edu.tw>
2019-12-23 22:10:46 +0800

    third commit

--- all.js
+++ all.js
@@ -1,14 +1,25 @@
 // 現在時間
 function clock() {
 var time = new Date(),
-    year = time.getFullYear(),
-    month = time.getMonth(),
-    day = time.getDate(),
-    hours = time.getHours(),
-    minutes = time.getMinutes(),
-    seconds = time.getSeconds();
+  year = time.getFullYear(),
+  month = time.getMonth(),
+  date = time.getDate(),
+  hours = time.getHours(),
+  minutes = time.getMinutes(),
+  seconds = time.getSeconds();
 
-document.querySelectorAll('.clock')[0].innerHTML = "現在時間是： "+ harold(year)+"-"+ harold(month+1)+"-"+harold(day)+" "+harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+var day = new Date();
+var weekday = new Array(7);
+  weekday[0] = "Sun";
+  weekday[1] = "Mon";
+  weekday[2] = "Tue";
+  weekday[3] = "Wed";
+  weekday[4] = "Thu";
+  weekday[5] = "Fri";
+  weekday[6] = "Sat";
+var n = weekday[day.getDay()];
+
+document.querySelector('.clock').innerHTML = "現在時間是： "+n+" "+ harold(year)+"-"+ harold(month+1)+"-"+harold(date)+" "+harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
 function harold(standIn) {
     if (standIn < 10) {
       standIn = '0' + standIn
@@ -40,8 +51,78 @@ function getData(){
 getData();
 
 // 判斷秀出 index+1, 店家, 營業時間 
-var table = document.querySelector('.table')
+var btn = document.querySelector('.btn');
+var tbody = document.querySelector('.tbody');
 
 function checkOpen(){
-  
+//判斷日期
+var day = new Date();
+var weekday = new Array(7);
+  weekday[0] = "Sun";
+  weekday[1] = "Mon";
+  weekday[2] = "Tue";
+  weekday[3] = "Wed";
+  weekday[4] = "Thu";
+  weekday[5] = "Fri";
+  weekday[6] = "Sat";
+var n = weekday[day.getDay()];
+// console.log(n);
+
+var array = [];
+  for(i=0; i<allData.length; i++){
+    // console.log(n);
+    var str = Object.keys(allData[i]);
+    // console.log(str);
+    for(j=0; j<8; j++){
+      if(n == str[j]){
+        var num = str.indexOf(n);
+      }
+    }// console.log(Object.values(allData[i])[num]); // 取得星期的營業時間
+
+// 判斷時間
+var time = new Date();
+  var hours = time.getHours();
+  var minutes = time.getMinutes();   
+  // var now = hours+ ":"+ minutes;
+  // console.log(now);
+  var status =  Object.values(allData[i])[num];
+  var newStatus = status.split(/-|:/); 
+  // console.log(newStatus);
+
+function add() {
+  array.push({
+    name: allData[i].name,
+    opentime: status    
+  });
+} 
+  if (newStatus[0] == hours && newStatus[1] <= minutes){
+    add();
+  } else if (newStatus[2] == hours && newStatus[3] >> minutes){
+    add();
+  } else if (newStatus[0] < hours && hours < newStatus[2] ){
+    add();
+  } else if ( newStatus[2]<10 ) {
+    if(newStatus[0] < hours && hours < newStatus[2]+24){
+    add();
+    } else if(hours == newStatus[2] && minutes < newStatus[3]){
+      add();
+    } else if(hours < newStatus[2]){
+      add();
+    }
+   } 
+
+};
+console.log(array);
+
+var str = '';
+for(i=0; i<array.length; i++){
+  str+=
+  '<tr><th scope="row">'+(i+1)+'</th>'+
+      '<td>'+array[i].name+'</td>'+
+      '<td>'+array[i].opentime+'</td></tr>';
 }
+
+tbody.innerHTML = str;
+} 
+
+btn.addEventListener('click',checkOpen,false);
